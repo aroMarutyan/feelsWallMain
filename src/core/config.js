f35a79cb554baf0dc5e2ef5cdd8964d095ae06ed
Deathrovv <aro.maru7ian@gmail.com>
2022-01-21 18:19:30 +0100

    putting firebase config file back. Needs to secure it in the firebase console

---
+++ src/core/config.js
@@ -0,0 +1,6 @@
+export const API_KEY = "AIzaSyDjgyLNtnol6-PggLgIqUKuHOfWpxX6Q-g";
+export const AUTH_DOMAIN = "sendamessageof.firebaseapp.com";
+export const PROJECT_ID = "sendamessageof";
+export const STORAGE_BUCKET = "sendamessageof.appspot.com";
+export const MESSAGING_SENDER_ID = "705239841314";
+export const APP_ID = "1:705239841314:web:c4661d098d4d1af1ece87b";
